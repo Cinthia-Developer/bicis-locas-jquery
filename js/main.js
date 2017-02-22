54c1e68a1049b51c2092cbe6ba2e476d907f886f
Cinthia <cinthiamashiel474@gmail.com>
2017-02-21 23:04:03 -0500

    obteniendo valores

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-function validateForm(){
-	/* Escribe tú código aquí */
-}
+$(document).ready(function(){
+    $("#submit").click(function(){
+        
+// Obtener los valores de los Inputs:
+        var name = $('#name').val();
+        var lastName = $('#lastname').val();
+        var email = $('#input-email').val();
+        var password = $('#input-password').val();
+        var selectBici = $('select')[0].selectedIndex; 
+        
